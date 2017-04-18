2200c33399142227c410969cf4b6e52977cfdee0
Kate Boyd <kwboyd95@gmail.com>
2017-04-18 17:41:54 -0400

    Initial commit.

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+use Illuminate\Http\Request;
+
+// Vocabs collection routes
+Route::get('/vocabs', 'VocabController@index');
+Route::post('/vocabs', 'VocabController@create');
+
+// Vocabs singular routes
+Route::get('/vocabs/{id}', 'VocabController@show');
+Route::put('/vocabs/{id}', 'VocabController@update');
+Route::delete('/vocabs/{id}', 'VocabController@destroy');
+
+
+Route::get('/', function () {
+  return view('welcome');
+});
